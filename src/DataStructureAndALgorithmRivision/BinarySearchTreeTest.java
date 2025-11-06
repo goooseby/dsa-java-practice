ec542b79410aca195f0807427a0d056b2b434653
looby <tryguend@qq.com>
2025-11-07 00:28:55 +0800

    finish BinarySearchTree

--- src/DataStructureAndALgorithmRivision/BinarySearchTreeTest.java
+++ src/DataStructureAndALgorithmRivision/BinarySearchTreeTest.java
@@ -1,8 +1,29 @@
 package DataStructureAndALgorithmRivision;
 
+import java.util.Random;
+
 public class BinarySearchTreeTest {
     public static void main(String[] args) {
         String greeting = "hello world";
         System.out.println(greeting);
+
+        BinarySearchTree bst = new BinarySearchTree();
+        Random rand = new Random();
+        rand.setSeed(47);
+
+        for (int i = 0; i < 50; i++) {
+            int a = rand.nextInt(100);
+            bst.insert(a);
+        }
+
+        bst.printTree();
+
+        System.out.println();
+        for (int i = 0; i < 50; i++) {
+            int a = rand.nextInt(100);
+            bst.remove(a);
+        }
+        bst.printTree();
+
     }
 }
