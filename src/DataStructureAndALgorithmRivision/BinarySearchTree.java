ec85f7a13d8ac772927ffad12c5ed18cd39931b5
looby <tryguend@qq.com>
2025-11-07 00:28:48 +0800

    finish BinarySearchTree

--- src/DataStructureAndALgorithmRivision/BinarySearchTree.java
+++ src/DataStructureAndALgorithmRivision/BinarySearchTree.java
@@ -6,7 +6,7 @@ public class BinarySearchTree {
     // 基本属性和构造方法
     private BinaryNode root;
 
-    private BinarySearchTree() {
+    public BinarySearchTree() {
         root = null;
     }
 
@@ -89,8 +89,16 @@ public class BinarySearchTree {
     // 之后是插入和删除：两个最重要的部分
     // 插入较为简单：只需要不断往下寻找到叶节点即可，如果已经存在，则直接跳过插入即可
     public void insert(double x){
-        insert(x, root);
+        root = insert(x, root);
     }
+
+    /**
+     * 真正的insert实现例程：
+     * 使用了递归的写法：每次插入，都是将新的子树接到该节点上
+     * @param x 需要插入的值
+     * @param t 插入的子树位置
+     * @return 返回新的子树给上面的节点
+     */
     private BinaryNode insert(double x, BinaryNode t){
         if (t == null){
             return new BinaryNode(x);
@@ -104,6 +112,72 @@ public class BinarySearchTree {
 
         return t;
     }
+
+    public void remove(double x){
+        remove(x, root);
+    }
+
+    /**
+     * remove 的真正实现
+     * 实现方法：递归
+     * @param x 需要删除的元素
+     * @param t 目标元素所在的子树
+     * @return 返回新子树，供处理后连接
+     */
+    private BinaryNode remove(double x, BinaryNode t){
+        // 到最后没有找到，返回空值
+        if (t == null){
+            return null;  // 为空，直接返回
+        }
+
+        // 不匹配：继续往后寻找
+        if (t.element > x){
+            t.left = remove(x, t.left); // 目标元素更小，往左找
+        }
+        else if (t.element < x){
+            t.right = remove(x, t.right);  // 更大往右
+        }
+        // 接下来是匹配的情况
+        // 如果该节点有两个子树
+        else if (t.left != null && t.right != null){
+            t.element = findMin(t.right).element;  // 换位：将右子树的最小值换位
+            t.right = remove(t.element, t.right);  // 删除被换的元素
+
+        }
+        // 只有一个子树
+        else
+            t = (t.left != null) ? t.left : t.right;
+        return t;
+    }
+
+    /**
+     * 按照升序，打印书的内容
+     */
+    public void printTree(){
+        if (isEmpty()){
+            System.out.println("Tree is empty");
+        }
+        else
+            printTree(root);
+    }
+
+    /**
+     * 实现printTree的内部方法
+     * 按顺序打印子树
+     * 实现方法：通过中序遍历
+     * @param t 一个子树的根节点
+     */
+    private void printTree(BinaryNode t){
+        if (t != null){
+            printTree(t.left);
+            System.out.print(t.element + " ");
+            printTree(t.right);
+        }
+    }
+
+
+
+
 
 
     /**
@@ -130,10 +204,5 @@ public class BinarySearchTree {
         }
 
 }
-
-
-
-
-
 
 }
